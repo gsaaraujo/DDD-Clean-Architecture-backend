6a5a50ee474f24204a96d8194a9d62d854817f9d
Gabriel Araujo <gabriel.houth@gmail.com>
2022-12-12 20:20:58 -0300

    fix: remove extra reference path of a deleted folder

--- src/modules/agreements/infra/controllers/make-an-agreement-controller.ts
+++ src/modules/agreements/infra/controllers/make-an-agreement-controller.ts
@@ -1,19 +1,19 @@
 import { Post, Body, Controller } from '@nestjs/common';
 import Joi from 'joi';
 
-import { Ok } from '../../../../shared/helpers/http/status-codes/ok';
-import { HttpResponse } from '../../../../shared/helpers/http/http-response';
-import { Conflict } from '../../../../shared/helpers/http/status-codes/conflict';
-import { NotFound } from '../../../../shared/helpers/http/status-codes/not-found';
-import { BadRequest } from '../../../../shared/helpers/http/status-codes/bad-request';
-import { InternalServerError } from '../../../../shared/helpers/http/status-codes/internal-server-error';
+import { Ok } from '../../../shared/helpers/http/status-codes/ok';
+import { HttpResponse } from '../../../shared/helpers/http/http-response';
+import { Conflict } from '../../../shared/helpers/http/status-codes/conflict';
+import { NotFound } from '../../../shared/helpers/http/status-codes/not-found';
+import { BadRequest } from '../../../shared/helpers/http/status-codes/bad-request';
+import { InternalServerError } from '../../../shared/helpers/http/status-codes/internal-server-error';
 
-import { IMakeAnAgreementUsecase } from '../../../domain/usecases/make-an-agreement-usecase';
+import { IMakeAnAgreementUsecase } from '../../domain/usecases/make-an-agreement-usecase';
 
 import {
   IMakeAnAgreementController,
   MakeAnAgreementControllerInput,
-} from '../../../adapters/controllers/make-an-agreement-controller';
+} from '../../adapters/controllers/make-an-agreement-controller';
 
 @Controller('agreements')
 export class MakeAnAgreementController implements IMakeAnAgreementController {
