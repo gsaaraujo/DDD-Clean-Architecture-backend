0b39fb726921a3865f38ccacbc55a152e796558d
Gabriel Araujo <gabriel.houth@gmail.com>
2022-12-27 11:40:20 -0300

    tests: add unit test to fake-party repository

--- jest.config.ts
+++ jest.config.ts
@@ -8,4 +8,8 @@ export default {
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '@core/(.*)': '<rootDir>/modules/core/$1',
+    '@agreements/(.*)': '<rootDir>/modules/agreements/$1',
+  },
 };
---
+++ src/modules/core/infra/fake/fake-party-repository.spec.ts
@@ -0,0 +1,35 @@
+import { FakePartyRepository } from '@core/infra/fake/fake-party-repository';
+
+describe('fake-party-repository', () => {
+  let fakePartyRepository: FakePartyRepository;
+
+  beforeEach(() => {
+    fakePartyRepository = new FakePartyRepository();
+  });
+
+  describe('exists', () => {
+    it('should return true with the given partyId', async () => {
+      fakePartyRepository.partiesIds = [
+        '0c87cf42-1369-4e9f-893d-9d0c7709b8b4',
+        'ac98db8a-be3f-47c1-ba8d-a52d96c05752',
+        '2fc98ddb-006c-43a6-9f1b-13d9c19a0158',
+      ];
+
+      const sut = await fakePartyRepository.exists('ac98db8a-be3f-47c1-ba8d-a52d96c05752');
+
+      expect(sut).toBeTruthy();
+    });
+
+    it('should return false with the given partyId', async () => {
+      fakePartyRepository.partiesIds = [
+        '0c87cf42-1369-4e9f-893d-9d0c7709b8b4',
+        'ac98db8a-be3f-47c1-ba8d-a52d96c05752',
+        '2fc98ddb-006c-43a6-9f1b-13d9c19a0158',
+      ];
+
+      const sut = await fakePartyRepository.exists('501f2325-755d-4195-b680-c4cc869617a8');
+
+      expect(sut).toBeFalsy();
+    });
+  });
+});
